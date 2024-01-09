ea5b81e968bea7208e37cfdc633512f17a60f8c2
Trugamr <trugamr@live.in>
2024-01-10 00:43:39 +0530

    feat(stacks): show stack status

--- app/lib/stack.server.ts
+++ app/lib/stack.server.ts
@@ -2,10 +2,16 @@ import fs from 'fs/promises'
 import path from 'path'
 import { env } from './env.server'
 import { execa } from 'execa'
+import { z } from 'zod'
 
-type Stack = {
+type StoredStack = {
   name: string
+  directory: string
   path: string
+}
+
+type ActiveStack = Omit<z.infer<typeof ActiveStackSchema>, 'status'> & {
+  status: 'running' | 'stopped'
 }
 
 export function getStacksDirectory() {
@@ -15,14 +21,14 @@ export function getStacksDirectory() {
 /**
  * Get list of child directories with `docker-compose.yml` present inside
  */
-export async function getStacksList() {
+export async function getStoredStacksList() {
   const stacksDirectory = getStacksDirectory()
 
   const entires = await fs.readdir(stacksDirectory, {
     withFileTypes: true,
   })
 
-  const stacks: Stack[] = []
+  const stacks: StoredStack[] = []
 
   for (const entry of entires) {
     if (entry.isDirectory()) {
@@ -32,11 +38,13 @@ export async function getStacksList() {
         const stats = await fs.stat(composePath)
         if (stats.isFile()) {
           const stackName = entry.name
-          const stackPath = path.resolve(stacksDirectory, stackName)
+          const stackDirectory = path.resolve(stacksDirectory, stackName)
+          const configPath = path.join(stackDirectory, 'docker-compose.yml')
 
           stacks.push({
             name: stackName,
-            path: stackPath,
+            directory: stackDirectory,
+            path: configPath,
           })
         }
       } catch (error) {
@@ -48,14 +56,14 @@ export async function getStacksList() {
   return stacks
 }
 
-export async function getStackByName(name: string) {
+export async function getStoredStackByName(name: string): Promise<StoredStack> {
   const stacksDirectory = getStacksDirectory()
-  const stackPath = path.resolve(stacksDirectory, name)
-  const composePath = path.join(stackPath, 'docker-compose.yml')
+  const stackDirectory = path.resolve(stacksDirectory, name)
+  const configPath = path.join(stackDirectory, 'docker-compose.yml')
 
   // Check if `docker-compose.yml` exists and is a file
   try {
-    const stats = await fs.stat(composePath)
+    const stats = await fs.stat(configPath)
     if (!stats.isFile()) {
       throw new Error(`Stack ${name} not found`)
     }
@@ -65,27 +73,103 @@ export async function getStackByName(name: string) {
 
   return {
     name,
-    path: stackPath,
+    directory: stackDirectory,
+    path: configPath,
   }
 }
 
-export async function startStack(stack: Stack) {
+export async function startStack(stack: StoredStack) {
   try {
     await execa('docker', ['compose', 'up', '-d'], {
-      cwd: stack.path,
+      cwd: stack.directory,
     })
   } catch (error) {
     throw new Error(`Failed to start stack ${stack.name}`)
   }
 }
 
-export async function stopStack(stack: Stack) {
+export async function stopStack(stack: StoredStack) {
   try {
     await execa('docker', ['compose', 'down', '--remove-orphans'], {
-      cwd: stack.path,
-      stdout: 'inherit',
+      cwd: stack.directory,
     })
   } catch (error) {
     throw new Error(`Failed to stop stack ${stack.name}`)
   }
+}
+
+// Example: running(2)
+const STACK_STATUS_REGEX = /^(?<current>\w+)\((?<count>\d+)\)$/
+
+const ActiveStackSchema = z
+  .object({
+    Name: z.string(),
+    Status: z.preprocess(
+      value => (typeof value === 'string' ? STACK_STATUS_REGEX.exec(value)?.groups : value),
+      z.object({
+        // TODO: Check correctness of this enum
+        current: z.enum(['running']),
+        count: z.coerce.number(),
+      }),
+    ),
+    ConfigFiles: z.string(),
+  })
+  .transform(values => {
+    return {
+      name: values.Name,
+      status: values.Status.current,
+      services: values.Status.count,
+      directory: path.dirname(values.ConfigFiles),
+      path: values.ConfigFiles,
+    }
+  })
+
+const ActiveStackListSchema = z.array(ActiveStackSchema)
+
+/**
+ * Get list of active stacks using
+ */
+export async function getActiveStacksList(): Promise<ActiveStack[]> {
+  let stdout: string
+  try {
+    const result = await execa('docker', ['compose', 'ls', '--format', 'json'])
+    stdout = result.stdout
+  } catch (error) {
+    throw new Error(`Failed to get active stacks`)
+  }
+
+  let json: unknown = null
+  try {
+    json = JSON.parse(stdout)
+  } catch (error) {
+    throw new Error(`Failed to parse active stacks JSON`)
+  }
+
+  return ActiveStackListSchema.parse(json)
+}
+
+/**
+ * Get stored stacks with their status
+ */
+export async function getStacksList() {
+  const [storedStacks, activeStacks] = await Promise.all([
+    getStoredStacksList(),
+    getActiveStacksList(),
+  ])
+  const stacks: ActiveStack[] = []
+
+  for (const storedStack of storedStacks) {
+    const activeStack = activeStacks.find(stack => stack.path === storedStack.path)
+    if (activeStack) {
+      stacks.push(activeStack)
+    } else {
+      stacks.push({
+        ...storedStack,
+        status: 'stopped',
+        services: 0,
+      })
+    }
+  }
+
+  return stacks
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,12 @@
 import { ActionFunctionArgs, json, type MetaFunction } from '@remix-run/node'
 import { Form, useLoaderData } from '@remix-run/react'
 import { z } from 'zod'
-import { getStackByName, getStacksList, startStack, stopStack } from '~/lib/stack.server'
-import { PackageIcon } from 'lucide-react'
+import { getStacksList, getStoredStackByName, startStack, stopStack } from '~/lib/stack.server'
+import { PackageIcon, PackageSearchIcon } from 'lucide-react'
 import { Table, TableBody, TableCell, TableRow } from '~/components/ui/table'
 import { Button } from '~/components/ui/button'
 import { Input } from '~/components/ui/input'
+import { cn } from '~/lib/utils'
 
 const StackFormSchema = z.object({
   stack: z.string(),
@@ -28,7 +29,7 @@ export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData()
   const values = StackFormSchema.parse(Object.fromEntries(formData))
 
-  const stack = await getStackByName(values.stack)
+  const stack = await getStoredStackByName(values.stack)
 
   switch (values.intent) {
     case 'start':
@@ -51,45 +52,87 @@ export default function IndexRoute() {
     <div className="bg-secondary min-h-screen">
       <header className="bg bg-background px-3 py-4 shadow">
         <h1 className="flex items-center gap-x-1 font-medium">
-          <PackageIcon className="h-[1.2em]" />
+          <PackageIcon className="h-[1.2em] w-[1.2em]" />
           <span>dabba</span>
         </h1>
       </header>
 
-      <main className="mx-auto max-w-6xl px-4 py-8">
+      <main className="mx-auto max-w-5xl px-4 py-8">
         <h2 className="text-3xl font-bold">Stacks</h2>
         <Form className="mt-8 flex gap-x-2">
           <Input placeholder="Search stacks..." />
           <Button>Search</Button>
         </Form>
-        <div className="bg-background mt-4 rounded-md border">
-          <Table>
-            <TableBody>
-              {stacks.map(stack => {
-                return (
-                  <TableRow>
-                    <TableCell>
-                      <span className="font-medium">{stack.name}</span>
-                      <span className="block text-sm text-gray-500">{stack.path}</span>
-                    </TableCell>
-                    <TableCell>
-                      <Form method="POST" className="ml-auto max-w-max">
-                        <input type="hidden" name="stack" value={stack.name} />
-                        <div className="flex gap-x-2">
-                          <Button name="intent" value="start" variant="outline" size="sm">
-                            Start
-                          </Button>
-                          <Button name="intent" value="stop" variant="outline" size="sm">
-                            Stop
-                          </Button>
+        <div className="bg-background mt-4 flex rounded-md border">
+          {stacks.length ? (
+            <Table>
+              <TableBody>
+                {stacks.map(stack => {
+                  return (
+                    <TableRow>
+                      <TableCell>
+                        <span className="font-medium">{stack.name}</span>
+                        <span className="block text-sm text-gray-500">{stack.directory}</span>
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex items-center gap-x-2">
+                          <div
+                            className={cn(
+                              'h-3 w-3 shrink-0 rounded-full',
+                              stack.status === 'running' ? 'bg-green-400' : 'bg-rose-400',
+                            )}
+                          />
+                          <div className="flex min-w-24 gap-x-1">
+                            {stack.status === 'running' ? (
+                              <span>Running</span>
+                            ) : (
+                              <span>Stopped</span>
+                            )}
+                            {stack.status === 'running' ? (
+                              <span>({stack.services})</span>
+                            ) : null}
+                          </div>
                         </div>
-                      </Form>
-                    </TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
+                      </TableCell>
+                      <TableCell>
+                        <Form method="POST" className="ml-auto max-w-max">
+                          <input type="hidden" name="stack" value={stack.name} />
+                          <div className="flex gap-x-2">
+                            {stack.status === 'stopped' ? (
+                              <Button
+                                name="intent"
+                                value="start"
+                                variant="outline"
+                                size="sm"
+                                className="min-w-16"
+                              >
+                                Start
+                              </Button>
+                            ) : (
+                              <Button
+                                name="intent"
+                                value="stop"
+                                variant="outline"
+                                size="sm"
+                                className="min-w-16"
+                              >
+                                Stop
+                              </Button>
+                            )}
+                          </div>
+                        </Form>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          ) : (
+            <div className="text-foreground/50 mx-auto flex flex-col items-center gap-y-2 p-8">
+              <PackageSearchIcon className="h-[2.4em] w-[2.4em]" />
+              <span>No stacks found</span>
+            </div>
+          )}
         </div>
       </main>
     </div>
